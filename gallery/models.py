96c86c739327625c8bf9d3a30c8e0201259e1c84
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-07 19:33:26 +0300

    adding model Location

--- gallery/models.py
+++ gallery/models.py
@@ -7,3 +7,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to='gallery/', default=None)
     image_name = models.CharField(max_length=30)
     image_description = models.TextField()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
