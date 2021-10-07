4a729290c74a48917127587834ca340565ac5469
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-07 19:05:08 +0300

    creating image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='gallery/', default=None)
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
