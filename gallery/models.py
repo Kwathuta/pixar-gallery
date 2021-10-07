2c81cca8374fa57205214cb961ce61a0fded7020
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-07 19:48:06 +0300

    adding relationships

--- gallery/models.py
+++ gallery/models.py
@@ -3,15 +3,18 @@ from django.db import models
 # Create your models here.
 
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='gallery/', default=None)
-    name = models.CharField(max_length=30)
-    description = models.TextField()
-
-
 class Location(models.Model):
     name = models.CharField(max_length=30)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='gallery/', default=None)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, default=None)
+    category = models.ManyToManyField(Category)
